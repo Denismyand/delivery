e6304692b3828b70e67f6cf936f53ab1b3b2a0fd
NVMate <denys.miand@nure.ua>
2022-07-05 22:29:20 +0300

    Created pages and linked them

--- delivery/src/App.js
+++ delivery/src/App.js
@@ -1,4 +1,25 @@
 import "./App.css";
+import Shop from "./Shop.js";
+import Cart from "./Cart.js";
+import { Routes, Route, Link } from "react-router-dom";
 
-export default function App() {}
+export default function App() {
+  return (
+    <div>
+      <Footer />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+}
 
+function Footer() {
+  return (
+    <>
+      <Link to="/">Shop</Link>
+      <Link to="cart">Cart</Link>
+    </>
+  );
+}
---
+++ delivery/src/Shop.js
@@ -0,0 +1,7 @@
+export default function Shop() {
+  return (
+    <>
+      <h1>This is the shop</h1>
+    </>
+  );
+}
