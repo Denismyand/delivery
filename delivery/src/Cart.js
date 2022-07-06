4506e0b7a1713a9be8b4b2457acd296937dacf77
NVMate <denys.miand@nure.ua>
2022-07-06 18:19:00 +0300

    Finished main non-functional interface components in Cart page

--- delivery/src/Cart.js
+++ delivery/src/Cart.js
@@ -1,3 +1,32 @@
+import { dishes } from "./Menu.js";
+import { useState } from "react";
+
 export default function Cart() {
-  return <h1>This is the cart</h1>;
+  return (
+    <div className="CartContent">
+      <div className="PersonalInfo">
+        <Input toinput="name" />
+        <Input toinput="email" />
+        <Input toinput="phone" />
+        <Input toinput="address" />
+      </div>
+      <div className="Cart"></div>
+      <div className="CartTotal">
+        <p>Total price:</p>
+      </div>
+        <button className="CartSubmit">Submit</button>
+    </div>
+  );
+}
+
+function Input({ toinput }) {
+  function Capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  return (
+    <div>
+      <p>{Capitalize(toinput) + ":"}</p>
+      <input placeholder={`Enter your ${toinput}`} />
+    </div>
+  );
 }
