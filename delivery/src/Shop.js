47d630bd20f61787c0685ea3d454f945ac831223
NVMate <denys.miand@nure.ua>
2022-07-06 04:36:08 +0300

    Finished main interface on the shop page (except for adding items to cart)

--- delivery/src/Shop.js
+++ delivery/src/Shop.js
@@ -1,31 +1,82 @@
 import { dishes } from "./Menu.js";
+import { useState } from "react";
 
 export default function Shop() {
+  let mcMenu = dishes.filter((dish) => dish.restaurant === "McDonny");
+  let cfkMenu = dishes.filter((dish) => dish.restaurant === "CFK");
+  let johnsMenu = dishes.filter((dish) => dish.restaurant === "Uncle John's");
+  let sonimodMenu = dishes.filter(
+    (dish) => dish.restaurant === "Sonimod Pizza"
+  );
+
+  const [menu, setMenu] = useState(mcMenu);
+  function chooseMenu(brand) {
+    setMenu(brand);
+  }
+
   return (
     <div className="ShopContent">
-      <Restaurants />
-      <Menu />
+      <Restaurants
+        chooseMenu={chooseMenu}
+        mcMenu={mcMenu}
+        cfkMenu={cfkMenu}
+        johnsMenu={johnsMenu}
+        sonimodMenu={sonimodMenu}
+      />
+      <Menu menu={menu} />
     </div>
   );
 }
 
-function Restaurants() {
+function Restaurants({ chooseMenu, mcMenu, cfkMenu, johnsMenu, sonimodMenu }) {
   return (
     <div className="RestaurantList">
       <h1>Shops:</h1>
-      <button className="RestaurantButton">McDonny</button>
-      <button className="RestaurantButton">CFK</button>
-      <button className="RestaurantButton">Uncle John's</button>
-      <button className="RestaurantButton">Sonimod Pizza</button>
+      <div className="RestaurantButtonBar">
+        <button className="RestaurantButton" onClick={() => chooseMenu(mcMenu)}>
+          McDonny
+        </button>
+        <button
+          className="RestaurantButton"
+          onClick={() => chooseMenu(cfkMenu)}
+        >
+          CFK
+        </button>
+        <button
+          className="RestaurantButton"
+          onClick={() => chooseMenu(johnsMenu)}
+        >
+          Uncle John's
+        </button>
+        <button
+          className="RestaurantButton"
+          onClick={() => chooseMenu(sonimodMenu)}
+        >
+          Sonimod Pizza
+        </button>
+      </div>
     </div>
   );
 }
 
-function Menu() {
+function Menu({ menu }) {
   return (
     <>
       <div className="MenuContent">
-        <h1>Menu</h1>
+        {menu.map((dish) => (
+          <div className="Dish" key={dish.id}>
+            <img
+              src={dish.image}
+              alt={dish.product}
+              width="100%"
+              height="83%"
+            />
+            <div>
+              <p className="DishName">{dish.product}</p>
+              <button className="AddToCart">Add to cart</button>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
